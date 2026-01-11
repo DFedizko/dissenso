dc612b46253f2df0e0ffe49749a045bb1b98f5fa
DFedizko <fedizkopedro@gmail.com>
2026-01-11 11:58:39 -0300

    chore: adding validation messages to article form schema

--- src/actions/article-schema.ts
+++ src/actions/article-schema.ts
@@ -2,9 +2,10 @@ import { z } from "astro/zod";
 
 export const createArticleSchema = z.object({
 	title: z
-		.string()
+		.string({ required_error: "É necessário ter um título no artigo." })
 		.min(3, "O título do artigo precisa ter pelo menos 3 caracteres.")
 		.max(100, "O título do artigo deve ter no máximo 100 caracteres."),
+
 	coverImage: z.string().optional(),
-	content: z.string(),
+	content: z.string({ required_error: "O conteúdo do artigo é necessário." }),
 });
